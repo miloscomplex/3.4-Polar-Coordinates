b3c855acebbcb2682d9180e232ee0bbb590b4c3c
Michael Sutton <hicallmesutton@gmail.com>
2023-03-07 13:24:13 -0600

    drawing of a circle with vertexes and rotating ball"

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 3.4-Polar-Coordinates
+
+in polar coordinates you can define the x and y coordinates by using the formula 
+
+x = cos(Θ) * r
+y = sin(Θ) * r
+
+polar coordinates are 
+(r,Θ) instead of (x,y)
--- main.js
+++ main.js
@@ -7,12 +7,11 @@ function setup() {
 }
 
 function draw() {
-  background(0, 20);
-  // stroke(255);
-  // strokeWeight(4);
-  // noFill();
+  background(0);
+  stroke(255);
+  noFill();
   translate(width/2, height/2)
-  // let r = 150;
+  let r = 150;
   //circle(0, 0, r * 2);
 
   strokeWeight(20);
@@ -20,7 +19,18 @@ function draw() {
   let x = r * cos(angle);
   let y = r * sin(angle);
   point(x, y);
-  
+
+  let increment = map(mouseX, 0, width, 0.01, PI);
+  //noLoop();
+  strokeWeight(4);
+  beginShape();
+  for (let i = 0; i < TWO_PI; i += increment) {
+    let r1 = r + random(-10, 10);
+    let x = r1 * cos(i);
+    let y = r1 * sin(i);
+    vertex(x,y);
+  }
+  endShape();
   angle += 0.02;
   r += random(-2, 2);
 }
